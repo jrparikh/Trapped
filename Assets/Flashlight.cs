1c49326e7a6eb25bc52641f126c3dfedb194e311
mrosales323 <manuelrosales91@gmail.com>
2018-01-21 14:57:54 -0800

    Add files via upload

---
+++ Assets/Flashlight.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+    public KeyCode flashlightToggleKey = KeyCode.F;
+    public float batteryLifeInSeconds = 60f;
+
+    public float maxIntensity = 1f;
+
+    private float batteryLife;
+    private bool isActive;
+
+    private Light myLight;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myLight = GetComponent<Light>();
+        batteryLife = myLight.intensity;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(flashlightToggleKey))
+        {
+            isActive = !isActive;
+        }
+        if (isActive)
+        {
+            myLight.enabled = true;
+            myLight.intensity -= batteryLife / batteryLifeInSeconds * Time.deltaTime;
+        }
+        else
+        {
+            myLight.enabled = false;
+        }
+    }
+
+    public void AddBatteryLife(float _batteryPower)
+    {
+        myLight.intensity += _batteryPower;
+        if (myLight.intensity > maxIntensity)
+            myLight.intensity = maxIntensity;
+    }
+}
