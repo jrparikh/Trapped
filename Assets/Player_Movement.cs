8ffc8e390e2266e322fff0599cf140a8f5ef1714
jrparikh <30482649+jrparikh@users.noreply.github.com>
2018-01-22 11:26:35 -0800

    We have player movement

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    public float maxSpeed = 10.0f;
+    bool facingRight = true;
+    int State = 0;
+
+    Animator anim;
+    Rigidbody2D RB;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        RB = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetKey("a"))
+        {
+            State = 3;
+            anim.SetInteger("State", State);
+            transform.position -= transform.right * Time.deltaTime * 4;
+            RB.velocity = new Vector3(0, RB.velocity.y, 0);
+            
+        }
+        else if (Input.GetKey("d"))
+        {
+            State = 3;
+            anim.SetInteger("State", State);
+            transform.position += transform.right * Time.deltaTime * 4;
+            RB.velocity = new Vector3(0, RB.velocity.y, 0);
+        }       
+        else if (Input.GetKey("w"))
+        {
+            State = 2;
+            anim.SetInteger("State", State);
+            transform.position += transform.forward * Time.deltaTime * 4;
+            
+            RB.velocity = new Vector2(0, RB.velocity.y);
+            State = 0;
+
+        }
+        else if (Input.GetKey("s"))
+        {
+            State = 1;
+            anim.SetInteger("State", State);
+            transform.position -= transform.forward * Time.deltaTime * 4;
+            
+            RB.velocity = new Vector2(0, RB.velocity.y);
+            //State = 1;
+        }
+        if(Input.anyKey == false && anim != null)
+        {
+            State = 0;
+            anim.SetInteger("State", State);
+        }
+        //if(Input.anyKey == true && anim != null)
+    
+
+        if (anim != null)
+        {
+            //anim.SetInteger("State", State);
+        }
+        else
+        {
+            //State = 0;
+        }
+
+        if (move > 0 && !facingRight ){
+            Flip();
+            
+        }else if(move < 0 && facingRight)
+        {
+            Flip();
+            
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
